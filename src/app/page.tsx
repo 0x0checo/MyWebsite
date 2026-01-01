6eb268d95a0d05bf771ec5f8cc788c8c11bf5268
Wenyu Li <wenyuli@h188-120-171-216.cust.bredband2.com>
2026-01-01 18:20:58 +0100

    change order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Page() {
 
       <main className="relative">
         <Hero />
+        <Highlights />
         <Projects />
-        <About />
-	<Highlights />
+	<About />
         <Contact />
 
         <footer className="py-10">
